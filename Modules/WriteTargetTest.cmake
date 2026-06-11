64de8518393e66acf3cf5316dea311016a698ce1
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-06-11 17:21:15 +1200

    Update framework to add pre and post test modifications to CMake based tests.

--- Modules/WriteTargetTest.cmake
+++ Modules/WriteTargetTest.cmake
@@ -1,10 +1,11 @@
-function(write_test _file _name _target _root _expected_results)
+function(write_test _file _name _target _root _expected_results _current_env)
     write_target_test(
         ${_file}
         ${_name}
         ${_target}
         ${_root}
         "${_expected_results}"
+        "${_current_env}"
         ${ARGN}
     )
 endfunction()
